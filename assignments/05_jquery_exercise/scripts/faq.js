2720e8a56d49d7fe8fe925f30e45840e2c6faf5e
ShoshanaS <shoshana.schultz@gmail.com>
2017-04-30 15:35:50 -0700

    In-class exercise complete

---
+++ assignments/05_jquery_exercise/scripts/faq.js
@@ -0,0 +1,20 @@
+ function showAnswer1() {
+  $('#answer1').slideToggle();
+}
+
+function showAnswer2() {
+  $('#answer2').slideToggle();  
+}
+
+function showAnswer3() {
+  $('#answer3').slideToggle();
+}
+
+// $('#answer1').hide();
+
+//Leverage classes to hide all answers
+$('.answer').hide();
+
+$('#question1').click(showAnswer1);
+$('#question2').click(showAnswer2);
+$('#question3').click(showAnswer3);
