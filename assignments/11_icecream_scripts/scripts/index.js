9fdbe5d9069eea3bdc1b88ab9f0458dd5997ec0d
ShoshanaS <shoshana.schultz@gmail.com>
2017-05-09 19:03:25 -0700

    Updated ice cream, optimized

--- assignments/11_icecream_scripts/scripts/index.js
+++ assignments/11_icecream_scripts/scripts/index.js
@@ -1,39 +1,61 @@
-function generateChoc() {
-  $('#yourflavor').text('chocolate');
+//function generateChoc() {
+  //$('#yourflavor').text('chocolate');
+//}
+
+//function generateRasp() {
+  //$('#yourflavor').text('raspberry');
+//}
+
+//function generateStraw() {
+  //$('#yourflavor').text('strawberry');
+//}
+
+//function generateMystery() {
+  //$('#yourflavor').text('mystery');
+//}
+
+// function TwoScoops() {
+//   $('#numberofscoops').text('2' + ' scoops ');
+// }
+
+// function ThreeScoops() {
+//   $('#numberofscoops').text('3' + ' scoops ');
+// }
+
+// function FourScoops() {
+//   $('#numberofscoops').text('4' + ' scoops ');
+// }
+
+//$('#chocolate').click(generateChoc);
+//$('#raspberry').click(generateRasp);
+//$('#strawberry').click(generateStraw);
+//$('#mystery').click(generateMystery);
+
+// $('#two').click(TwoScoop s);
+// $('#three').click(ThreeScoops);
+// $('#four').click(FourScoops);
+
+
+function setNumScoops() {
+  // What scoop choice was clicked?
+  var chosenScoop = event.target;
+  // What is the scoop amount?
+  var numScoops = $(chosenScoop).text();
+
+  // Find scoop holder element and update its contents
+  $('#numberofscoops').text(numScoops + ' scoops ');
 }
 
-function generateRasp() {
-  $('#yourflavor').text('raspberry');
+$('#scoops li').click(setNumScoops);
+
+function setFlavor() {
+  var chosenFlavor = event.target;
+  var typeFlavor = $(chosenFlavor).text();
+  $('#yourflavor').text(chosenFlavor);
 }
 
-function generateStraw() {
-  $('#yourflavor').text('strawberry');
-}
+$('#flavor li').click(setFlavor);
 
-function generateMystery() {
-  $('#yourflavor').text('mystery');
-}
-
-function TwoScoops() {
-  $('#numberofscoops').text('2' + ' scoops ');
-}
-
-function ThreeScoops() {
-  $('#numberofscoops').text('3' + ' scoops ');
-}
-
-function FourScoops() {
-  $('#numberofscoops').text('4' + ' scoops ');
-}
-
-$('#chocolate').click(generateChoc);
-$('#raspberry').click(generateRasp);
-$('#strawberry').click(generateStraw);
-$('#mystery').click(generateMystery);
-
-$('#two').click(TwoScoops);
-$('#three').click(ThreeScoops);
-$('#four').click(FourScoops);
 
 
 
